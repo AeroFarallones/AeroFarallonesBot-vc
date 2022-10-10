e6404be85ef0778c69f5f1043c401796bca2eb95
rcomunica <julir2772@gmail.com>
2022-10-10 00:11:12 -0500

    “update”

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,22 @@
-const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./jsconfig.json');
 
-const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
-	.map(command => command.toJSON());
+const { SlashCommandBuilder } = require('@discordjs/builders')
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("faq")
+        .setDescription("Send a FAQ question and answer in selected channel")
+        .addChannelOption(channel => {
+            channel
+                .setName("channel")
+                .setDescription("Channel you want to send the FAQ embed in")
+                .setRequired(true)
+        }),
+    async execute (interaction) {
+        await interaction.reply("Pong! :ping_pong:")
+    }
+}
 const rest = new REST({ version: '10' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
